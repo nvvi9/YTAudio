024e29b5a945dd11e45087abe6996fcf90173837
nvie <nikita.nm32@gmail.com>
2020-06-03 18:25:44 +0300

    room updated

--- app/src/main/java/com/example/ytaudio/MainActivityViewModel.kt
+++ app/src/main/java/com/example/ytaudio/MainActivityViewModel.kt
@@ -2,14 +2,9 @@ package com.example.ytaudio
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.commit451.youtubeextractor.YouTubeExtractor
 import com.example.ytaudio.database.AudioDatabaseDao
 
 class MainActivityViewModel(
     val database: AudioDatabaseDao,
     application: Application
-) : AndroidViewModel(application) {
-
-    private val extractor = YouTubeExtractor.Builder().build()
-
-}
+) : AndroidViewModel(application)
