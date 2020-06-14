5fbafafd1fade228fdc24b087fa4c69be46e2121
nvvi9 <nvvi9@protonmail.com>
2020-06-14 15:06:29 +0300

    updating database only if needed

--- app/src/main/java/com/example/ytaudio/screens/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/ytaudio/screens/playlist/PlaylistViewModel.kt
@@ -2,11 +2,9 @@ package com.example.ytaudio.screens.playlist
 
 import android.app.Application
 import android.widget.Toast
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.ytaudio.database.AudioDatabaseDao
+import com.example.ytaudio.database.AudioInfo
 import com.example.ytaudio.utils.LiveContentException
 import com.example.ytaudio.utils.getAudioInfo
 import com.example.ytaudio.utils.needUpdate
@@ -26,7 +24,16 @@ class PlaylistViewModel(
     private val viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    val audioPlaylist = database.getAllAudio()
+    private val _audioPlaylist = database.getAllAudio()
+
+    val audioPlaylist = Transformations.map(_audioPlaylist) { playlist ->
+        playlist?.forEach {
+            if (it.needUpdate) {
+                updateItem(it)
+            }
+        }
+        playlist
+    }
 
     fun onExtract(youtubeUrl: String) {
         val youtubeId = youtubeUrl.takeLastWhile { it != '=' && it != '/' }
@@ -70,9 +77,16 @@ class PlaylistViewModel(
         }
     }
 
-    init {
-        updateDatabase()
+    private fun updateItem(audioInfo: AudioInfo) {
+        uiScope.launch {
+            audioInfo.updateInfo()
+            database.update(audioInfo)
+        }
     }
+
+//    init {
+//        updateDatabase()
+//    }
 
     private fun showToast(message: String?) =
         Toast.makeText(getApplication(), message ?: "error", Toast.LENGTH_SHORT).show()
