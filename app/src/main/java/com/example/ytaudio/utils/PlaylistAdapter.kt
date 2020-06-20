bae7212f9bb93b7ee76834f41a858d85b6f4c3f6
nvvi9 <nvvi9@protonmail.com>
2020-06-20 13:49:58 +0300

    onBindViewHolder updated to display current playback state

--- app/src/main/java/com/example/ytaudio/utils/PlaylistAdapter.kt
+++ app/src/main/java/com/example/ytaudio/utils/PlaylistAdapter.kt
@@ -18,14 +18,31 @@ class PlaylistAdapter(private val clickListener: AudioInfoListener) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(clickListener, getItem(position))
+        onBindViewHolder(holder, position, mutableListOf())
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        holder.bind(clickListener, getItem(position), payloads)
+    }
 
     inner class ViewHolder(private val binding: ItemPlaylistBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(clickListener: AudioInfoListener, item: AudioItem) {
-            binding.audioItem = item
+        fun bind(clickListener: AudioInfoListener, item: AudioItem, payloads: MutableList<Any>) {
+            var refresh = payloads.isEmpty()
+
+            if (payloads.isNotEmpty()) {
+                payloads.forEach {
+                    when (it) {
+                        PLAYBACK_STATE_CHANGED -> binding.playbackState.setImageResource(item.playbackStatus)
+                        else -> refresh = true
+                    }
+                }
+            }
+
+            if (refresh) {
+                binding.audioItem = item
+            }
+
             binding.clickListener = clickListener
             binding.executePendingBindings()
         }
@@ -42,10 +59,12 @@ class AudioInfoDiffCallback : DiffUtil.ItemCallback<AudioItem>() {
         oldItem == newItem
 
     override fun getChangePayload(oldItem: AudioItem, newItem: AudioItem) =
-        if (oldItem.playbackStatus != newItem.playbackStatus) 1 else null
+        if (oldItem.playbackStatus != newItem.playbackStatus) PLAYBACK_STATE_CHANGED else null
 }
 
 
 class AudioInfoListener(val clickListener: (audioItem: AudioItem) -> Unit) {
     fun onClick(audioItem: AudioItem) = clickListener(audioItem)
 }
+
+const val PLAYBACK_STATE_CHANGED = 1
