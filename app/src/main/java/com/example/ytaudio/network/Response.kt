487f1ecb1988d8a5efe1150b3d6ccbb7f2c8fdac
nvvi9 <nvvi9@protonmail.com>
2020-06-24 00:00:12 +0300

    ApiService started

---
+++ app/src/main/java/com/example/ytaudio/network/Response.kt
@@ -0,0 +1,44 @@
+package com.example.ytaudio.network
+
+data class Response(
+    val kind: String,
+    val etag: String,
+    val nextPageToken: String,
+    val regionCode: String,
+    val pageInfo: PageInfo,
+    val items: List<VideoItem>
+)
+
+data class PageInfo(
+    val totalResults: Int,
+    val resultsPerPage: Int
+)
+
+data class VideoItem(
+    val kind: String,
+    val etag: String,
+    val id: Id,
+    val snippet: Snippet
+)
+
+data class Id(
+    val kind: String,
+    val videoId: String
+)
+
+data class Snippet(
+    val publishedAt: String,
+    val channelId: String,
+    val title: String,
+    val description: String,
+    val thumbnails: List<Thumbnail>,
+    val channelTitle: String,
+    val liveBroadcastContent: String,
+    val publishTime: String
+)
+
+data class Thumbnail(
+    val url: String,
+    val width: Int,
+    val height: Int
+)
