851381e793b9f8c9c8abb786fd2923c057ee82ca
nvvi9 <nvvi9@protonmail.com>
2021-01-20 15:18:09 +0300

    v1.3

--- app/src/main/java/com/nvvi9/ytaudio/repositories/paging/YTVideoDetailsPagingSource.kt
+++ app/src/main/java/com/nvvi9/ytaudio/repositories/paging/YTVideoDetailsPagingSource.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import javax.inject.Inject
 
-
 @FlowPreview
 @ExperimentalCoroutinesApi
 class YTVideoDetailsPagingSource @Inject constructor(
@@ -21,7 +20,7 @@ class YTVideoDetailsPagingSource @Inject constructor(
     private val ytStreamDataSource: YTStreamDataSource
 ) : PagingSource<String, YTVideoDetails>() {
 
-    override suspend fun load(params: LoadParams<String>) =
+    override suspend fun load(params: LoadParams<String>): LoadResult<String, YTVideoDetails> =
         ytNetworkDataSource.getPopular(params.loadSize, params.key).run {
             when (this) {
                 is Result.Success -> {
